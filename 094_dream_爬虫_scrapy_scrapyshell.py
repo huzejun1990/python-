78ae7675c2022c64664f2c20f55f4833d6a59eb4
huzejun <huzejun1990>
2023-01-27 02:28:46 +0800

    初次上传 python爬虫文件 scrapy爬取 scrapyshell安装、配置、使用 2023-1-27 02:28:39

---
+++ 094_dream_爬虫_scrapy_scrapyshell.py
@@ -0,0 +1,7 @@
+# @Author : huzejun
+# @Time : 2023/1/25 19:34
+
+# 进入到 scrapy shell的终端 直接在windows的终端中输入 scrapy shell 域名
+# 如果想看到一些高亮 或者 自动补全 那么可以安装ipython pip install ipython
+
+# scrapy shell www.baidu.com
